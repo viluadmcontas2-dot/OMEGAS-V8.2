47ec9b338ce5c257c861265bd9398a249816f5ad
viluadmcontas2-dot <viluadmcontas2@gmail.com>
2026-08-14 16:58:47 -0300

    feat: add bounded visual-pressure signal to NEXT scheduler

--- app/src/main/assets/ui-next/core/scheduler.js
+++ app/src/main/assets/ui-next/core/scheduler.js
@@ -3,6 +3,9 @@ export class NextScheduler {
   #running = false;
   #rafId = 0;
   #lastAt = 0;
+  #previousFrameAt = 0;
+  #pressureScore = 0;
+  #pressure = 'NORMAL';
 
   addHook(id, callback, minIntervalMs = 0) {
     if (!id || typeof callback !== 'function') throw new TypeError('hook NEXT inválido');
@@ -14,6 +17,7 @@ export class NextScheduler {
   start() {
     if (this.#running) return;
     this.#running = true;
+    this.#previousFrameAt = 0;
     this.#rafId = requestAnimationFrame((time) => this.#tick(time));
   }
 
@@ -28,17 +32,31 @@ export class NextScheduler {
     return this.#hooks.size;
   }
 
+  pressure() {
+    return this.#pressure;
+  }
+
   debugSnapshot() {
     return Object.freeze({
       running: this.#running,
       hookCount: this.#hooks.size,
       lastAt: this.#lastAt,
+      pressure: this.#pressure,
+      pressureScore: this.#pressureScore,
       hooks: [...this.#hooks.keys()],
     });
   }
 
   #tick(time) {
     if (!this.#running) return;
+    if (this.#previousFrameAt > 0) {
+      const frameGap = time - this.#previousFrameAt;
+      if (frameGap > 80) this.#pressureScore = Math.min(10, this.#pressureScore + 2);
+      else if (frameGap > 48) this.#pressureScore = Math.min(10, this.#pressureScore + 1);
+      else if (frameGap < 36) this.#pressureScore = Math.max(0, this.#pressureScore - 1);
+      this.#pressure = this.#pressureScore >= 3 ? 'REDUCED' : 'NORMAL';
+    }
+    this.#previousFrameAt = time;
     this.#lastAt = time;
     for (const hook of this.#hooks.values()) {
       if (time - hook.lastRunAt < hook.minIntervalMs) continue;
