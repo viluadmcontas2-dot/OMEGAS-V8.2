6585384a705c21102853fa75f48ac1fe7300ca60
viluadmcontas2-dot <viluadmcontas2@gmail.com>
2026-08-18 02:50:20 -0300

    test: require reference timing in selector json

--- app/src/test/java/com/omegas/prohub/learning/ReferenceAvailabilityTrackerTest.kt
+++ app/src/test/java/com/omegas/prohub/learning/ReferenceAvailabilityTrackerTest.kt
@@ -35,4 +35,24 @@ class ReferenceAvailabilityTrackerTest {
         assertEquals(0L, blocked.timeToReferenceMs)
         assertEquals(50L, available.timeToReferenceMs)
     }
+
+    @Test
+    fun selectorJsonPublishesTimingStateAndBlockReason() {
+        val metric = ReferenceAvailabilityMetric(
+            state = "BLOCKED",
+            timeToReferenceMs = 321L,
+            blockReason = "NO_LOCAL_PETROL_REFERENCE",
+        )
+        val json = PetrolReferenceSelector.Result(
+            available = false,
+            reasonCode = "NO_LOCAL_PETROL_REFERENCE",
+            message = "blocked",
+            referenceAvailability = metric,
+        ).toJson()
+
+        assertEquals("BLOCKED", json.getString("reference_wait_state"))
+        assertEquals(321L, json.getLong("time_to_reference_ms"))
+        assertEquals("NO_LOCAL_PETROL_REFERENCE", json.getString("reference_block_reason"))
+        assertEquals("FIRST_BLOCKED_REFERENCE_ATTEMPT", json.getString("reference_timing_origin"))
+    }
 }
