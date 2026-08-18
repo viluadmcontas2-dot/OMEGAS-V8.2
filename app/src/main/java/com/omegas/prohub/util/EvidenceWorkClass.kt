bc62b0fb63c27d6161a92b024dfa90b8cef7c6d2
viluadmcontas2-dot <viluadmcontas2@gmail.com>
2026-08-18 04:51:40 -0300

    runtime: type marginal information value for 055A

--- app/src/main/java/com/omegas/prohub/util/EvidenceWorkClass.kt
+++ app/src/main/java/com/omegas/prohub/util/EvidenceWorkClass.kt
@@ -1,6 +1,22 @@
 package com.omegas.prohub.util
 
 /**
+ * Classe qualitativa do ganho marginal de informação esperado.
+ *
+ * Isto não é probabilidade, confiança nem magnitude científica. É somente uma
+ * ordenação operacional explícita para o backpressure escolher o que preservar
+ * quando o trabalho downstream satura. O custo real continua sendo observado
+ * por queue-age, processing time e thread CPU no consumer.
+ */
+enum class MarginalInformationClass {
+    DIAGNOSTIC_PRESENT_STATE,
+    REUSABLE_REFERENCE,
+    CONTEXT_COHERENT_OBSERVATION,
+    FAST_OBJECTIVE_OBSERVATION,
+    CAUSAL_POST_INTERVENTION,
+}
+
+/**
  * Valor semântico do trabalho científico depois que o frame já foi adquirido.
  *
  * Isto não classifica nem controla aquisição MP48. A porta/telemetria ficam fora
@@ -8,13 +24,18 @@ package com.omegas.prohub.util
  */
 enum class EvidenceWorkClass(
     val valueRank: Int,
+    val marginalInformationClass: MarginalInformationClass,
     val diagnosticOnly: Boolean = false,
 ) {
-    DIAGNOSTIC_ONLY(10, diagnosticOnly = true),
-    STATIC_REFERENCE(40),
-    DYNAMIC_COHERENT(60),
-    FAST_KSTAR(80),
-    POST_WRITE_REVALIDATION(100),
+    DIAGNOSTIC_ONLY(
+        10,
+        MarginalInformationClass.DIAGNOSTIC_PRESENT_STATE,
+        diagnosticOnly = true,
+    ),
+    STATIC_REFERENCE(40, MarginalInformationClass.REUSABLE_REFERENCE),
+    DYNAMIC_COHERENT(60, MarginalInformationClass.CONTEXT_COHERENT_OBSERVATION),
+    FAST_KSTAR(80, MarginalInformationClass.FAST_OBJECTIVE_OBSERVATION),
+    POST_WRITE_REVALIDATION(100, MarginalInformationClass.CAUSAL_POST_INTERVENTION),
 }
 
 object EvidenceBackpressurePolicy {
