94d69beb64694f633b97c5a7e3eb53618e652d4e
viluadmcontas2-dot <viluadmcontas2@gmail.com>
2026-09-01 07:59:34 -0300

    test(wu006): scope imported RED science lab

---
+++ lab/red_blend/__init__.py
@@ -0,0 +1,5 @@
+"""WU-006 offline science subset reused from RED Science Blend.
+
+This package is replay/falsification instrumentation only. It is not project
+authority, Android runtime authority, ECU authority or vehicle proof.
+"""
